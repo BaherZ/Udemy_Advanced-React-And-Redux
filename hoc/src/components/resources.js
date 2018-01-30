b270b595687d005875467c5655b765bbe05d0a28
Baher <mbaher18@gmail.com>
2018-01-30 16:03:52 +0200

    higher order components

---
+++ hoc/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default ()=>{
+    return (
+        <div>
+            Super special recipe
+            <ul>
+                <li>ing1</li>
+                <li>ing2</li>
+                <li>ing3</li>
+            </ul>
+        </div>
+    )
+}
